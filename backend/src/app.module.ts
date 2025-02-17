d2380e68232b7109ee29fffe3026f0414f22c1c2
Joclelson Rodrigues <jocleelson.159@hotmail.com>
2025-02-17 14:47:48 -0300

    feat: :sparkles: Criação do modulo de carteiras com os endpoints de criação e busca

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AssetsModule } from './assets/assets.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { WalletsModule } from './wallets/wallets.module';
 
 const uri =
   'mongodb://root:root@localhost:27017/hbroker?authSource=admin&directConnection=true';
 
 @Module({
-  imports: [MongooseModule.forRoot(uri), AssetsModule],
+  imports: [MongooseModule.forRoot(uri), AssetsModule, WalletsModule],
   controllers: [AppController],
   providers: [AppService],
 })
