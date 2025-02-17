dbdb880d5d30ff669c593607cac0c4b3d94794a8
Joclelson Rodrigues <jocleelson.159@hotmail.com>
2025-02-17 14:24:29 -0300

    feat: :sparkles: Criação do modulo Assets com os endpoints POST, GET

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AssetsModule } from './assets/assets.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const uri =
+  'mongodb://root:root@localhost:27017/hbroker?authSource=admin&directConnection=true';
 
 @Module({
+  imports: [MongooseModule.forRoot(uri), AssetsModule],
   controllers: [AppController],
   providers: [AppService],
 })
