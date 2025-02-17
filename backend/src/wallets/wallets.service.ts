f546853486c10771b5e3b4e00bd82b9408f48317
Joclelson Rodrigues <jocleelson.159@hotmail.com>
2025-02-17 15:55:23 -0300

    feat: :sparkles: Criação do endpoint que adiciona um ativo a carteira

--- backend/src/wallets/wallets.service.ts
+++ backend/src/wallets/wallets.service.ts
@@ -1,12 +1,18 @@
-import { Injectable } from '@nestjs/common';
-import { CreateWalletDto } from './dto/create-wallet.dto';
 import { Model } from 'mongoose';
-import { Wallet } from './entities/wallet.entity';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { Wallet } from './entities/wallet.entity';
+import { CreateWalletDto } from './dto/create-wallet.dto';
+import { CreateWalletAssetDto } from './dto/create-wallet-asset.dto';
+import { WalletAsset } from './entities/wallet-asset.entity';
 
 @Injectable()
 export class WalletsService {
-  constructor(@InjectModel(Wallet.name) private walletSchema: Model<Wallet>) {}
+  constructor(
+    @InjectModel(Wallet.name) private walletSchema: Model<Wallet>,
+    @InjectModel(WalletAsset.name)
+    private walletAssetSchema: Model<WalletAsset>,
+  ) {}
 
   create(createWalletDto: CreateWalletDto) {
     return this.walletSchema.create(createWalletDto);
@@ -18,6 +24,14 @@ export class WalletsService {
 
   findOne(id: string) {
     return this.walletSchema.findById(id);
+  }
+
+  createWalletAsset(data: CreateWalletAssetDto) {
+    return this.walletAssetSchema.create({
+      wallet: data.walletId,
+      asset: data.assetId,
+      shares: data.shares,
+    });
   }
 
   // update(id: number, updateWalletDto: UpdateWalletDto) {
